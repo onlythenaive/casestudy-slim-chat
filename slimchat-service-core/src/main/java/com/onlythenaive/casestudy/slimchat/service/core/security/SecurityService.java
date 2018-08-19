95eb64b38733b451d4408f56a6247ef7f18249e2
onlythenaive <ilya.gubarev@gmail.com>
2018-08-19 20:55:12 +0300

    Security service added

---
+++ slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/security/SecurityService.java
@@ -0,0 +1,30 @@
+package com.onlythenaive.casestudy.slimchat.service.core.security;
+
+/**
+ * Security service.
+ *
+ * @author Ilia Gubarev
+ */
+public interface SecurityService {
+
+    /**
+     * Creates a new security account.
+     *
+     * @param nickname account nickname.
+     * @param password account password.
+     */
+    void createAccount(String nickname, String password);
+
+    /**
+     * Creates a new authentication and security token.
+     *
+     * @param nickname account nickname.
+     * @param password account password.
+     */
+    void login(String nickname, String password);
+
+    /**
+     * Removes current authentication and security token.
+     */
+    void logout();
+}
---
+++ slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/security/SecurityServiceBean.java
@@ -0,0 +1,81 @@
+package com.onlythenaive.casestudy.slimchat.service.core.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.onlythenaive.casestudy.slimchat.service.core.generic.GenericComponentBean;
+import com.onlythenaive.casestudy.slimchat.service.core.security.account.Account;
+import com.onlythenaive.casestudy.slimchat.service.core.security.account.AccountService;
+import com.onlythenaive.casestudy.slimchat.service.core.security.authentication.Authentication;
+import com.onlythenaive.casestudy.slimchat.service.core.security.authentication.AuthenticationContextConfigurator;
+import com.onlythenaive.casestudy.slimchat.service.core.security.password.PasswordService;
+import com.onlythenaive.casestudy.slimchat.service.core.security.token.Token;
+import com.onlythenaive.casestudy.slimchat.service.core.security.token.TokenService;
+
+/**
+ * Security service.
+ *
+ * @author Ilia Gubarev
+ */
+@Service
+public class SecurityServiceBean extends GenericComponentBean implements SecurityService {
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private AuthenticationContextConfigurator authenticationContextConfigurator;
+
+    @Autowired
+    private PasswordService passwordService;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    public void createAccount(String nickname, String password) {
+        Account account = this.accountService.createAccount(nickname, password);
+        createNewAuthenticationAndToken(account);
+    }
+
+    @Override
+    public void login(String nickname, String password) {
+        Account account = this.accountService.findAccountByNickname(nickname);
+        if (account == null) {
+            throw loginFailed();
+        }
+        boolean verified = this.passwordService.verify(password, account.getPasswordHash());
+        if (!verified) {
+            throw loginFailed();
+        }
+        createNewAuthenticationAndToken(account);
+    }
+
+    @Override
+    public void logout() {
+        removeCurrentAuthenticationAndToken();
+    }
+
+    private void createNewAuthenticationAndToken(Account account) {
+        String nickname = account.getNickname();
+        Token token = this.tokenService.createToken(nickname);
+        Authentication authentication = Authentication.builder()
+                .account(account)
+                .token(token)
+                .build();
+        this.authenticationContextConfigurator.setAuthentication(authentication);
+    }
+
+    private void removeCurrentAuthenticationAndToken() {
+        Authentication authentication = this.authenticationContextConfigurator.getAuthentication();
+        if (authentication != null) {
+            Token token = authentication.getToken();
+            this.tokenService.removeTokenById(token.getId());
+            this.authenticationContextConfigurator.setAuthentication(null);
+        }
+    }
+
+    private RuntimeException loginFailed() {
+        return new RuntimeException("Login attempt failed");
+    }
+}
