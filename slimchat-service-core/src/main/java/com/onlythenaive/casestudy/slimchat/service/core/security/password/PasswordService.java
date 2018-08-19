fc18512b028951dd8776f216b499ebfb8226fadf
onlythenaive <ilya.gubarev@gmail.com>
2018-08-19 20:17:01 +0300

    Security account password service added

---
+++ slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/security/password/PasswordService.java
@@ -0,0 +1,26 @@
+package com.onlythenaive.casestudy.slimchat.service.core.security.password;
+
+/**
+ * Security account password service.
+ *
+ * @author Ilia Gubarev
+ */
+public interface PasswordService {
+
+    /**
+     * Generates a hash from specified account password.
+     *
+     * @param password account password.
+     * @return password hash.
+     */
+    String hash(String password);
+
+    /**
+     * Verifies specified account password against provided hash.
+     *
+     * @param password account password to be verified.
+     * @param passwordHash password hash.
+     * @return true if verification is successful.
+     */
+    boolean verify(String password, String passwordHash);
+}
---
+++ slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/security/password/PasswordServiceBean.java
@@ -0,0 +1,25 @@
+package com.onlythenaive.casestudy.slimchat.service.core.security.password;
+
+import org.springframework.stereotype.Service;
+
+import com.onlythenaive.casestudy.slimchat.service.core.generic.GenericComponentBean;
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ * Security account password service implementation.
+ *
+ * @author Ilia Gubarev
+ */
+@Service
+public class PasswordServiceBean extends GenericComponentBean implements PasswordService {
+
+    @Override
+    public String hash(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    @Override
+    public boolean verify(String password, String passwordHash) {
+        return BCrypt.checkpw(password, passwordHash);
+    }
+}
