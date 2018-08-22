6920530042283dc565cf41a84647ba0dfd073cc6
onlythenaive <ilya.gubarev@gmail.com>
2018-08-22 18:58:27 +0300

    Group provider service updated

--- slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/domain/group/GroupProvider.java
+++ slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/domain/group/GroupProvider.java
@@ -1,6 +1,10 @@
 package com.onlythenaive.casestudy.slimchat.service.core.domain.group;
 
+import java.util.Optional;
+
 public interface GroupProvider {
 
     Group getGroup(String id);
+
+    Optional<Group> findGroup(String id);
 }
--- slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/domain/group/GroupProviderBean.java
+++ slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/domain/group/GroupProviderBean.java
@@ -1,5 +1,7 @@
 package com.onlythenaive.casestudy.slimchat.service.core.domain.group;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,11 @@ public class GroupProviderBean extends GenericDomainComponentBean implements Gro
         return this.groupRepository.findById(id)
                 .map(this::project)
                 .orElseThrow(this::groupNotFound);
+    }
+
+    @Override
+    public Optional<Group> findGroup(String id) {
+        return this.groupRepository.findById(id).map(this::project);
     }
 
     private Group project(GroupEntity entity) {
