cc08f91f0c014c7830ca878852f1ca79635d40b7
onlythenaive <ilya.gubarev@gmail.com>
2018-08-22 20:16:51 +0300

    Message repository .findAllById(...) method overridden

--- slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/domain/message/MessageRepository.java
+++ slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/domain/message/MessageRepository.java
@@ -1,7 +1,11 @@
 package com.onlythenaive.casestudy.slimchat.service.core.domain.message;
 
+import java.util.Collection;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface MessageRepository extends MongoRepository<MessageEntity, String> {
 
+    @Override
+    Collection<MessageEntity> findAllById(Iterable<String> ids);
 }
