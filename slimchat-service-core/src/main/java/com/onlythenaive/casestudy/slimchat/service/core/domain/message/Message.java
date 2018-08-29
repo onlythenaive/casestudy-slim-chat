8c5db6504ade2d714e3854dde8b6ee47740b2663
onlythenaive <ilya.gubarev@gmail.com>
2018-08-29 03:34:47 +0300

    Message/chat history view improved

--- slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/domain/message/Message.java
+++ slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/domain/message/Message.java
@@ -1,6 +1,12 @@
 package com.onlythenaive.casestudy.slimchat.service.core.domain.message;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,11 +30,25 @@ public class Message {
     private String id;
     private String text;
     private Profile author;
+    private Boolean writtenByPrincipal;
     private Profile recipient;
     private Group group;
     private Instant createdAt;
 
     public boolean isAffiliatedToGroup() {
         return this.group != null;
+    }
+
+    public String getPrettyTimestamp() {
+        LocalDateTime datetime = LocalDateTime.ofInstant(createdAt, ZoneId.systemDefault());
+        LocalDate date = datetime.toLocalDate();
+        LocalTime time = datetime.toLocalTime();
+        String timeString = time.format(DateTimeFormatter.ofPattern("hh:mm"));
+        if (date.isBefore(LocalDate.now())) {
+            String dateString = date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+            return timeString + " " + dateString;
+        } else {
+            return timeString;
+        }
     }
 }
--- slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/domain/message/MessageProjectorBean.java
+++ slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/domain/message/MessageProjectorBean.java
@@ -29,6 +29,7 @@ public class MessageProjectorBean extends GenericComponentBean implements Messag
                 .id(entity.getId())
                 .text(entity.getText())
                 .author(profile(entity.getAuthorId()))
+                .writtenByPrincipal(entity.getAuthorId().equals(principalId()))
                 .recipient(profile(entity.getRecipientId()))
                 .group(group(entity.getGroupId()))
                 .createdAt(entity.getCreatedAt())
