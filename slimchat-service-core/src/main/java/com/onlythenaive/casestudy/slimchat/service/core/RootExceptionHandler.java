db6f9f623e0890c45c5ac19fa55d4f2a026e3b71
onlythenaive <ilya.gubarev@gmail.com>
2018-09-03 05:40:44 +0300

    Root exception handler improved to catch Operation exceptions

--- slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/RootExceptionHandler.java
+++ slimchat-service-core/src/main/java/com/onlythenaive/casestudy/slimchat/service/core/RootExceptionHandler.java
@@ -2,14 +2,32 @@ package com.onlythenaive.casestudy.slimchat.service.core;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import com.onlythenaive.casestudy.slimchat.service.core.utility.component.GenericComponentBean;
+import com.onlythenaive.casestudy.slimchat.service.core.utility.exception.ExceptionDescriptor;
+import com.onlythenaive.casestudy.slimchat.service.core.utility.exception.ExceptionDescriptorService;
+import com.onlythenaive.casestudy.slimchat.service.core.utility.exception.OperationException;
+
 @ControllerAdvice
-public class RootExceptionHandler {
+public class RootExceptionHandler extends GenericComponentBean {
+
+    @Autowired
+    private ExceptionDescriptorService exceptionDescriptorService;
+
+    @ExceptionHandler(OperationException.class)
+    public Object handleOperationException(OperationException exception) {
+        logger().debug("Operation exception occurred", exception);
+        ExceptionDescriptor descriptor = this.exceptionDescriptorService.extract(exception);
+        return ResponseEntity
+                .status(descriptor.getCategory().getDefaultHttpStatus())
+                .body(descriptor);
+    }
 
     @ExceptionHandler(NoHandlerFoundException.class)
     public Object handleNoHandlerFoundException(Exception exception, HttpServletRequest request) {
